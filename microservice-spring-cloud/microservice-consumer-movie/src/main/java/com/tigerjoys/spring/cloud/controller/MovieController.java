b1e769d18310f523c2d1a43076bf3e04f7106e9e
chengang <172577882@qq.com>
2018-05-12 15:47:15 +0800

    Eureka示例

---
+++ microservice-spring-cloud/microservice-consumer-movie/src/main/java/com/tigerjoys/spring/cloud/controller/MovieController.java
@@ -0,0 +1,22 @@
+package com.tigerjoys.spring.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.tigerjoys.spring.cloud.entity.User;
+
+@RestController
+public class MovieController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@GetMapping("/movie/{id}")
+	public User findById(@PathVariable Long id) {
+		return this.restTemplate.getForObject("http://127.0.0.1:7900/simple/" + id, User.class);
+	}
+
+}
---
+++ microservice-spring-cloud/microservice-provider-user/src/main/java/com/tigerjoys/spring/cloud/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.tigerjoys.spring.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.tigerjoys.spring.cloud.entity.User;
+import com.tigerjoys.spring.cloud.repository.UserRepository;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private EurekaClient eurekaClient;
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
+	@GetMapping("/simple/{id}")
+	public User findById(@PathVariable Long id) {
+		return this.userRepository.findOne(id);
+	}
+	
+	/**
+	 * 如何使用EurekaClient来进行一些操作，如从Eureka中获取服务的主页等
+	 * @return String
+	 */
+	@GetMapping("/eureka-instance")
+	public String serviceURL() {
+		InstanceInfo instance = this.eurekaClient.getNextServerFromEureka("MICROSERVICE-PROVIDER-USER", false);
+		return instance.getHomePageUrl();
+	}
+	
+	/**
+	 * 获得本机的服务信息
+	 * @return ServiceInstance
+	 */
+	@GetMapping("/instance-info")
+	public ServiceInstance instanceInfo() {
+		ServiceInstance instance = this.discoveryClient.getLocalServiceInstance();
+		return instance;
+	}
+
+}
