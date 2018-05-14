8220ddf35cbf0fb0e3e263d2ed73575f44952011
chengang <172577882@qq.com>
2018-05-14 21:33:10 +0800

    Feign覆写默认配置以及设置Feign日志

--- microservice-spring-cloud/microservice-consumer-movie-feign-customizing/src/main/java/com/tigerjoys/spring/cloud/controller/MovieController.java
+++ microservice-spring-cloud/microservice-consumer-movie-feign-customizing/src/main/java/com/tigerjoys/spring/cloud/controller/MovieController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tigerjoys.spring.cloud.client.FeignClient2;
 import com.tigerjoys.spring.cloud.client.UserFeignClient;
 import com.tigerjoys.spring.cloud.entity.User;
 
@@ -14,9 +15,17 @@ public class MovieController {
 	@Autowired
 	private UserFeignClient userFeignClient;
 	
+	@Autowired
+	private FeignClient2 feignClient2;
+	
 	@GetMapping("/movie/{id}")
 	public User findById(@PathVariable Long id) {
 		return this.userFeignClient.findById(id);
+	}
+	
+	@GetMapping("/{serviceName}")
+	public String findEurekaByServiceName(@PathVariable String serviceName) {
+		return this.feignClient2.findEurekaByServiceName(serviceName);
 	}
 
 }
--- microservice-spring-cloud/microservice-consumer-movie-feign-customizing/src/main/java/com/tigerjoys/spring/config/FeignConfiguration2.java
+++ microservice-spring-cloud/microservice-consumer-movie-feign-customizing/src/main/java/com/tigerjoys/spring/config/FeignConfiguration2.java
@@ -3,25 +3,14 @@ package com.tigerjoys.spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import feign.Contract;
-import feign.Logger;
+import feign.auth.BasicAuthRequestInterceptor;
 
 @Configuration
-public class FeignConfiguration {
+public class FeignConfiguration2 {
 	
 	@Bean
-    public Contract feignContract() {
-        return new feign.Contract.Default();
-    }
-	
-	@Bean
-    Logger.Level feignLoggerLevel() {
-        return Logger.Level.FULL;
-    }
-
-    /*@Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
         return new BasicAuthRequestInterceptor("user", "password");
-    }*/
+    }
 
 }
